121733f71a122f06b7d96a42700be8ca206282c5
MathLeCodeur <mathieu.pires@edu.esiee.fr>
2019-05-09 19:11:15 +0200

    Modification du insert pour ne pas insert en boucle,mais seulement les images nouvelles dans un repertoire

--- src/kappa/controllers/ImageController.py
+++ src/kappa/controllers/ImageController.py
@@ -3,7 +3,10 @@ from kappa.controllers.Controller import Controller
 from kappa.dao import ConnectionManager
 from kappa.models.ImageModel import ImageModel
 from kappa.dao.ImageDAO import ImageDAO
-
+import os
+import glob
+from PIL import Image
+import time
 
 class ImageController(Controller):
 	def __init__(self):
@@ -23,26 +26,24 @@ class ImageController(Controller):
 		self.cDao.linkToVector(imgModel,vector)
 
 	def importImageFolder(self,pathF):
-		y = ConnectionManager.ConnectionManager('KappaBase.db')
+
 		l=os.listdir(pathF)
 
 		#get next id
-		ui=y.executeSQL("select Max(id_image) from Image")
-		u=ui
-		for elem in ui:
-			if(elem[0] == None):
-				u=0
-				break
-			u=elem[0]+1
+		u=self.cDao.getNextId()
+
+		listImage = self.cDao.getAll()
+		listPath =[]
+		for im in listImage:
+			listPath.append(im.path)
 
 		#file in folder
 		for i in l:
-			print(i)
-			if(os.path.isfile(pathF+i)):
+			pathName = pathF+i
+			if(os.path.isfile(pathName) and pathName not in listPath):
 
 				extension = i.split(".")[1]
 				if(extension in ("jpeg","jpg","png","PNG","JPEG","JPG")):
-
 					im = Image.open(pathF+str(i))
 					path = pathF+str(i)
 					size = os.path.getsize(path)
@@ -50,6 +51,6 @@ class ImageController(Controller):
 					height = im.size[1]
 					date = str(time.ctime(os.path.getctime(pathF+str(i))))
 					sql	= "Insert into IMAGE (id_image,creation_date ,length, width,size, path) values ("+str(u)+",'"+date+"',"+str(height)+", "+str(width)+ ", " +str(size)+", '" +path+"')"
-					print(path)
-					#y.executeAndCommitSQL(sql)
+					print("image insert" + path)
+					y.executeAndCommitSQL(sql)
 					u+=1
