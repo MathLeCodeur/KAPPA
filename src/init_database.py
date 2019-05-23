739f98fed8b8c8abcbaada68c01e13c1a1ac7665
viennem <maxime.vienne@outlook.com>
2019-05-23 18:26:39 +0200

    Final changes.

---
+++ src/init_database.py
@@ -0,0 +1,8 @@
+from kappa.controllers.ObjectVectorController import ObjectVectorController
+
+def main():
+	controller = ObjectVectorController()
+	controller.importObjectVectors()
+
+if __name__ == "__main__":
+    main()
